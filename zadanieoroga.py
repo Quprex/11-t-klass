5aae1a05df4c074b274775a1c7089ef68f5ffbe7
Quprex <andrejznamensikov204@gmail.com>
2025-11-23 19:31:15 +0300

    Add files via upload

---
+++ zadanieoroga.py
@@ -0,0 +1,9 @@
+final = 342 
+maxStart = 0 
+
+for k in range(1001):  
+    startZeros = final + k - 1  
+    if startZeros <= 999: 
+        maxStart = max(maxStart, startZeros) 
+
+print(maxStart) 
