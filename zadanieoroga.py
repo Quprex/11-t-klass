96743ca72736eac8d41237d481ec62cacbf6aac7
Quprex <andrejznamensikov204@gmail.com>
2025-11-23 19:42:49 +0300

    Update zadanieoroga.py

--- zadanieoroga.py
+++ zadanieoroga.py
@@ -1,10 +1,15 @@
-final = 343 
-maxStart = 0 
-
-for k in range(1001):  
-    startZeros = final + k - 1  
-    if startZeros <= 999: 
-        maxStart = max(maxStart, startZeros) 
-
-print(maxStart) 
-
+total_length = 1000
+final_zeros = 343
+tape = [0] * total_length
+possible_zero_counts = []
+for pos in range(total_length):
+    current_tape = tape.copy()
+    current_tape[pos] = 1
+    zeros_left = pos
+    zeros_right = total_length - pos - 1
+    remaining_zeros_right = final_zeros - zeros_left
+    if 0 <= remaining_zeros_right <= zeros_right:
+        total_initial_zeros = zeros_left + zeros_right
+        possible_zero_counts.append(total_initial_zeros)
+max_initial_zeros = max(possible_zero_counts)
+print("Максимальное количество нулей в исходной последовательности:", max_initial_zeros)
